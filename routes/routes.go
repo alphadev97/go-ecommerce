99a63f4f60b89070978c8af620d8f84cf85ee757
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-07-13 22:31:25 +0500

    routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,10 +1,14 @@
 package routes
 
 import (
+	"github.com/alphadev97/go-ecommerce/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.POST("users/signup")
-	incomingRoutes.POST("users/login")
+	incomingRoutes.POST("/users/signup", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.Login())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())
 }
