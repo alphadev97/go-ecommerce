56b4dd3e12b9bfddc86f125e8ea758f6bffc6b0b
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-07-20 18:29:48 +0500

    InstantBuy

--- controllers/cart.go
+++ controllers/cart.go
@@ -100,4 +100,38 @@ func GetItemFromCart() gin.HandlerFunc
 
 func BuyFromCart() gin.HandlerFunc
 
-func InstantBuy() gin.HandlerFunc
+func (app *Application) InstantBuy() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		productQueryID := c.Query("id")
+		if productQueryID == "" {
+			log.Println("product id is empty")
+
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("product id is empty"))
+			return
+		}
+
+		userQueryID := c.Query("userID")
+		if userQueryID == "" {
+			log.Println("user id is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("user id is empty"))
+			return
+		}
+
+		productID, err := primitive.ObjectIDFromHex(productQueryID)
+		if err != nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		err = database.InstantBuyer(ctx, app.prodCollection, app.userCollection, productID, userQueryID)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, err)
+		}
+
+		c.IndentedJSON(200, "successfully place the order")
+	}
+}
