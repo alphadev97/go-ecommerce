020dcbe83194a009fba36c3c1a221af63e043006
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-07-21 01:13:43 +0500

    TokenGenerator

--- tokens/tokengen.go
+++ tokens/tokengen.go
@@ -1,1 +1,58 @@
 package token
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/alphadev97/go-ecommerce/database"
+	jwt "github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type SignedDetails struct {
+	Email      string
+	First_Name string
+	Last_Name  string
+	Uid        string
+	jwt.StandardClaims
+}
+
+var UserData *mongo.Collection = database.UserData(database.Client, "Users")
+
+var SECRET_KEY = os.Getenv("SECRET_KEY")
+
+func TokenGenerator(email string, firstname string, lastname string, uid string) (signedtoken string, signedrefereshtoken string, err error) {
+	claims := &SignedDetails{
+		Email:      email,
+		First_Name: firstname,
+		Last_Name:  lastname,
+		Uid:        uid,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	refereshclaims := &SignedDetails{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodES256, claims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", "", err
+	}
+
+	refereshtoken, err := jwt.NewWithClaims(jwt.SigningMethodES256, refereshclaims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	return token, refereshtoken, err
+}
+
+func ValidateToken()
+
+func UpdateAllTokens()
